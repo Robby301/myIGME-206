dd7890627b52ecde83969ef056d43a4388cf1204
roble <roble@LAPTOP-852PA4LK>
2021-08-27 21:29:05 -0400

    Final HelloWorld

--- Disbrow_HelloWorld/Program.cs
+++ Disbrow_HelloWorld/Program.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//The statement belows allows the access to the system namespace for easier coding within the program
+using System;
 
+//Below is the namespace for the HelloWorld project/assignment
 namespace Disbrow_HelloWorld
 {
+    //The "class program" statement simply creates a class called program that within can be used to write the core of this assignment
     class Program
     {
+        //The next statement is a method called the main method where the code will be written
         static void Main(string[] args)
         {
+            //The next two statements declare integer variables num1 and num2 so that math calculations can be made
             int num1 = 1 + 2;
             int num2 = 3;
 
+            //The next statement prints the sum of num1 and num2 into the console
             Console.WriteLine("Num 1 + Num 2 = " + (num1 + num2) + "\n");
 
+            //This final part is a for loop that prints the statement "Hello World!" six times (or the number of times the sum of num1 and num2 equals)
             for(int i = 0; i < (num1 + num2); i++)
             {Console.WriteLine("Hello World!");}
         }
