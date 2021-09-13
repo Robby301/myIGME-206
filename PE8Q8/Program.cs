06c4526797d1a6291b1cfd54b1b445856321b170
roble <roble@LAPTOP-852PA4LK>
2021-09-13 18:21:04 -0400

    PE8Q8

---
+++ PE8Q8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Q8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "No, I have nothing in my purse!";
+
+            sentence = sentence.Replace("No", "Yes");
+            sentence = sentence.Replace("no", "yes");
+
+            Console.WriteLine(sentence);
+        }
+    }
+}
