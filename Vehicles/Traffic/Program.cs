cd1943e7f8ed1e34cb315b3f816ca40fe806e173
roble <roble@LAPTOP-852PA4LK>
2021-09-27 23:59:53 -0400

    Traffic

---
+++ Vehicles/Traffic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Vehicles;
+
+namespace Traffic
+{
+    // Class: Program
+    // Author: Robert Gregory Disbrow
+    // Purpose: Test the implementation of vehicles as a reference
+    // Restrictions: None
+    class Program
+    {
+        public void AddPassenger(IPassengerCarrier vehicleObject)
+        {
+            vehicleObject.LoadPassenger();
+
+            Console.WriteLine(vehicleObject.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            FreightTrain testTrain = new FreightTrain();
+            SUV testSUV = new SUV();
+
+            AddPassenger(testTrain);
+            AddPassenger(SUV);
+        }
+    }
+}
