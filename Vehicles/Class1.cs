be99e894f81330716b59292e5dfa3b447f138373
roble <roble@LAPTOP-852PA4LK>
2021-09-27 23:42:15 -0400

    Vehicles

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// namespace: Vehicles
+// Author: Robert Gregory Disbrow
+// Purpose: Has multiple abstract classes, public classes and interfaces that all interact with inheritance
+// Restrictions: None
+namespace Vehicles
+{
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+    public interface IHeavyLoadCarrier { }
+
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    public abstract class Car : Vehicle { }
+    public class Compact : Car, IPassengerCarrier { }
+    public class SUV : Car, IPassengerCarrier { }
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier { }
+
+    public abstract class Train : Vehicle { }
+    public class PassengerTrain : Train, IPassengerCarrier { }
+    public class FreightTrain : Train, IHeavyLoadCarrier { }
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier { }
+}
