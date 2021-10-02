f4fdf784f15bfcbb3d3d7a97aa58e8187d709092
roble <roble@LAPTOP-852PA4LK>
2021-10-01 21:43:44 -0400

    PE12Q3

---
+++ PE12Q3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PE12Q3
+{
+	public class MyClass
+	{
+		private string myString;
+
+		public string MyString
+		{
+			set
+			{
+				myString = "Robby";
+			}
+		}
+
+		public virtual string GetString()
+		{
+			return myString;
+		}
+	}
+
+	public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + " (output from the derived class)";
+		}
+    }
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			MyDerivedClass test = new MyDerivedClass();
+			Console.WriteLine(test.GetString());
+        }
+    }
+}
