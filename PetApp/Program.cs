5019cdbb301460313438615fa55b9cad4f06b2fd
roble <roble@LAPTOP-852PA4LK>
2021-10-04 14:42:05 -0400

    PE13FirstDraft

---
+++ PetApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetApp
+{
+    public abstract class Pet
+    {
+        private string name;
+        public int age;
+
+        public Pet()
+        {
+
+        }
+        public Pet(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name;
+        public abstract void Eat();
+        public abstract void Play();
+        public abstract void GotoVet();
+    }
+    public class Pets
+    {
+        List<Pet> petList = new List<Pet>();
+
+        public Pet petEI
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
