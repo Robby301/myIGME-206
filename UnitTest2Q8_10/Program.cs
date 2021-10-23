50ceb42693818123e89cc5b953be47009c293554
roble <roble@LAPTOP-852PA4LK>
2021-10-22 23:53:08 -0400

    UnitTest2Q8_10(WITH COMMENTS)

--- UnitTest2Q8_10/Program.cs
+++ UnitTest2Q8_10/Program.cs
@@ -2,6 +2,11 @@
 
 namespace UnitTest2Q8_10
 {
+    // Class: Game
+    // Author: Robert Gregory Disbrow
+    // Purpose: The game class is abstract, and is the parent class of a few of the other classes in the program. It includes a variable, a property, an abstract
+    //          method and a virtual method.
+    // Restrictions: None
     public abstract class Game
     {
         public string title;
@@ -23,6 +28,11 @@ namespace UnitTest2Q8_10
             Console.WriteLine("You have stopped playing the game.");
         }
     }
+    // Interface: VideoGameInterface
+    // Author: Robert Gregory Disbrow
+    // Purpose: This interface sets up the template for classes that can be categorized as video games (which will be seen later in the program)
+    //          The interface includes a property and a void method.
+    // Restrictions: None
     public interface VideoGameInterface
     {
         string VideoGameConsole
@@ -32,6 +42,11 @@ namespace UnitTest2Q8_10
         }
         void View();
     }
+    // Interface: BoardGameInterface
+    // Author: Robert Gregory Disbrow
+    // Purpose: This interface sets up the template for classes that can be categorized as board games (which will be seen later in the program)
+    //          The interface includes one property.
+    // Restrictions: None
     public interface BoardGameInterface
     {
         int NumOfPieces
@@ -41,6 +56,11 @@ namespace UnitTest2Q8_10
         }
     }
 
+    // Class: RegularVideoGame
+    // Author: Robert Gregory Disbrow
+    // Purpose: The RegularVideoGame class is the child of the Game class and implements the VideoGameInterface interface
+    //          The view and play methods both have a Console.WriteLine output that helps later in the program to test for polymorphism
+    // Restrictions: None
     public class RegularVideoGame : Game, VideoGameInterface
     {
         public string videoGameConsole;
@@ -65,6 +85,11 @@ namespace UnitTest2Q8_10
             Console.WriteLine("Time to play a video game!");
         }
     }
+    // Class: VRVideoGame
+    // Author: Robert Gregory Disbrow
+    // Purpose: The VRVideoGame class is the child of the Game class and implements the VideoGameInterface interface
+    //          The view and play methods both have a Console.WriteLine output that helps later in the program to test for polymorphism
+    // Restrictions: None
     public class VRVideoGame : Game, VideoGameInterface
     {
         public string videoGameConsole;
@@ -89,6 +114,10 @@ namespace UnitTest2Q8_10
             Console.WriteLine("Time to play a virtual reality game!");
         }
     }
+    // Class: BoardGame
+    // Author: Robert Gregory Disbrow
+    // Purpose: The BoardGame class is the child of the Game class and implements the BoardGameInterface interface
+    // Restrictions: None
     public class BoardGame : Game, BoardGameInterface
     {
         public int numOfPieces;
@@ -110,8 +139,15 @@ namespace UnitTest2Q8_10
         }
     }
 
+    // Class: Program
+    // Author: Robert Gregory Disbrow
+    // Purpose: The program class is where the program will be tested for polymorphism
+    // Restrictions: None
     class Program
     {
+        // Method: CheckGameDisplay
+        // Purpose: This method handles checking to see if the program has polymorphism
+        // Restrictions: None
         static void CheckGameDisplay(object obj)
         {
             if (obj.GetType() == typeof(RegularVideoGame))
@@ -125,6 +161,9 @@ namespace UnitTest2Q8_10
                 gameInterface.View();
             }
         }
+        // Method: Main
+        // Purpose: The main is used to test the polymorphism of the program
+        // Restrictions: None
         static void Main(string[] args)
         {
             RegularVideoGame metroid = new RegularVideoGame();
