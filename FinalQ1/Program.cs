4020ff26154ad21a355f67335d726120b01fa081
roble <roble@LAPTOP-852PA4LK>
2021-12-15 21:57:16 -0500

    FinalQ1(COMPLETE)

---
+++ FinalQ1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FinalQ1
+{
+    // Class: Program
+    // Author: Robert Gregory Disbrow
+    // Purpose: This program will take a user's inputted sentence and output how much of each alphabetical letter appears in that sentence
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: The main is where the program performs the operations to get the user sentence and output how much of each letter appears in the sentence
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            string sentence;
+            string letter;
+            int[] letterCount = new int[26];
+
+            Random rand = new Random();
+
+            Console.Write("Enter your sentence: ");
+            sentence = Console.ReadLine();
+
+            //This foreach loop handles getting the count/amount of how much each letter appears in the sentence by using the char value of lowercase "a" as a
+            //pointer to each index of each letter in the alphabet (with 0 being a, 1 being b and so on until 25 is z (the 26th letter)).
+            //That's done by subtracting the letter's char value by the char value of "a"
+            foreach (char c in sentence.ToLower())
+            {
+                if (char.IsLetter(c))
+                {
+                    ++letterCount[c - 'a'];
+                }
+            }
+
+            Console.WriteLine("Character counts:");
+
+            //This for loop is what outputs the information of how much each letter appears in the user's sentence
+            for (int i = 0; i < letterCount.Length; ++i)
+            {
+                letter = ((char)(i + 'a')).ToString();
+                Console.WriteLine(letter.ToUpper() + ": " + letterCount[i]);
+            }
+        }
+    }
+}
---
+++ FinalQ2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalQ2
+{
+    class Program
+    {
+        //Adjaceny Matrix
+        static int[,] adjMatrix = new int[,]
+        {
+            //       A    B    C    D    E    F    G    H
+            /*A*/  {-1,   1,   5,  -1,  -1,  -1,  -1,  -1},
+            /*B*/  {-1,  -1,  -1,   1,  -1,   7,  -1,  -1},
+            /*C*/  {-1,  -1,  -1,   0,   2,  -1,  -1,  -1},
+            /*D*/  {-1,   1,   0,  -1,  -1,  -1,  -1,  -1},
+            /*E*/  {-1,  -1,   2,  -1,  -1,  -1,   2,  -1},
+            /*F*/  {-1,  -1,  -1,  -1,  -1,  -1,  -1,   4},
+            /*G*/  {-1,  -1,  -1,  -1,   2,   1,  -1,  -1},
+            /*H*/  {-1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}
+        };
+
+        // Adjacency List (neighbor's index, weight)
+        static (int, int)[][] listGraph = new (int, int)[][]
+        {
+            /* A (0) */ new (int, int)[] {(1, 1), (2, 5)},
+            /* B (1) */ new (int, int)[] {(3, 1), (5, 7)},
+            /* C (2) */ new (int, int)[] {(3, 0), (4, 2)},
+            /* D (3) */ new (int, int)[] {(1, 1), (2, 0)},
+            /* E (4) */ new (int, int)[] {(2, 2), (6, 2)},
+            /* F (5) */ new (int, int)[] {(7, 4)},
+            /* G (6) */ new (int, int)[] {(4, 2), (5,1)},
+            /* H (7) */ null
+        };
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
